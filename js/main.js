652428d092e02c3b1505af35fe9a3130757082aa
Sir-Ross <43891611+Sir-Ross@users.noreply.github.com>
2022-03-14 14:48:24 -0400

    almost connected

--- js/main.js
+++ js/main.js
@@ -14,5 +14,21 @@ const client = new MongoClient(uri,
 	});
 
 
+async function run() {
+  try {
+    // Connect the client to the server
+    console.log("Connecting");
+    await client.connect();
+    console.log("Connecting to database");
+    // Establish and verify connection
+    await client.db("campusdate").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    console.log("Connection closed");
+    await client.close();
+  }
+}
 
-
+run();
+//console.log("Test");
